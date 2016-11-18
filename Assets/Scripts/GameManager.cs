7ddbe24eff6b43f56770874ed6ae082340bf35e0
eyesniper2 <eyesniper2@rayfall.net>
2016-11-17 21:44:24 -0800

    Socket IO is now working and touch controllers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,15 @@ public class GameManager : MonoBehaviour
        0 = Lobby (Open and close connections)
        1 = Game has started 
     */
+    public GameObject leftHand;
+    public GameObject rightHand;
+    public Material handReady;
+    public Material handNotReady;
     int CurrentGameState = 0;
     int PlayersConnected = 0;
+    bool leftReady = false;
+    bool rightReady = false;
+    bool hasSentPrep = false;
 
     private SocketIOComponent socket;
 
@@ -20,14 +27,59 @@ public class GameManager : MonoBehaviour
         socket = go.GetComponent<SocketIOComponent>();
         socket.On("error", SocketError);
         socket.On("close", SocketClose);
-        socket.On("player_connect", PlayerConnect);
-        socket.On("player_dc", PlayerDisconnect);
+        socket.On("lobby_update", LobbyUpdate);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (CurrentGameState == 0)
+        {
+            //Check Left hand
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) >= 0.3 && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch) >= 0.3)
+            {
+                if (!leftReady)
+                {
+                    //Change texture
+                    leftHand.GetComponent<Renderer>().material = handReady;
+                    leftReady = true;
+                }
+            }
+            else
+            {
+                if (leftReady)
+                {
+                    leftHand.GetComponent<Renderer>().material = handNotReady;
+                    leftReady = false;
+                }
+            }
+            //Check Right hand
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch) >= 0.3 && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) >= 0.3)
+            {
+                if (!rightReady)
+                {
+                    //Change texture
+                    rightHand.GetComponent<Renderer>().material = handReady;
+                    rightReady = true;
+                }
+            }
+            else
+            {
+                if (rightReady)
+                {
+                    rightHand.GetComponent<Renderer>().material = handNotReady;
+                    rightReady = false;
+                }
+            }
+            //Check if game is ready to start!
+            if(rightReady && leftReady && PlayersConnected > 0)
+            {
+                //Start game here
+                CurrentGameState = 1;
 
+            }
+        }
+        
     }
 
 
@@ -40,13 +92,8 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
     }
-    public void PlayerConnect(SocketIOEvent e)
+    public void LobbyUpdate(SocketIOEvent e)
     {
-        
-        Debug.Log("[SocketIO] Player connect received: " + e.name + " " + e.data);
-    }
-    public void PlayerDisconnect(SocketIOEvent e)
-    {
-        Debug.Log("[SocketIO] Player disconnect received: " + e.name + " " + e.data);
+        Debug.Log("[SocketIO] Lobby Update: " + e.name + " " + e.data);
     }
 }
