2023c6ce74209a3b7745867dc6e285db72e56e9d
eyesniper2 <eyesniper2@rayfall.net>
2016-11-17 16:38:38 -0800

    Cleaned up files and started on manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class GameManager : MonoBehaviour
+{
+    /* Keep track of current game state
+       0 = Lobby (Open and close connections)
+       1 = Game has started 
+    */
+    int CurrentGameState = 0;
+    int PlayersConnected = 0;
+
+    private SocketIOComponent socket;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject go = GameObject.Find("SocketIO");
+        socket = go.GetComponent<SocketIOComponent>();
+        socket.On("error", SocketError);
+        socket.On("close", SocketClose);
+        socket.On("player_connect", PlayerConnect);
+        socket.On("player_dc", PlayerDisconnect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void SocketError(SocketIOEvent e)
+    {
+        Debug.Log("[SocketIO] Error received: " + e.name + " " + e.data);
+    }
+
+    public void SocketClose(SocketIOEvent e)
+    {
+        Debug.Log("[SocketIO] Close received: " + e.name + " " + e.data);
+    }
+    public void PlayerConnect(SocketIOEvent e)
+    {
+        
+        Debug.Log("[SocketIO] Player connect received: " + e.name + " " + e.data);
+    }
+    public void PlayerDisconnect(SocketIOEvent e)
+    {
+        Debug.Log("[SocketIO] Player disconnect received: " + e.name + " " + e.data);
+    }
+}
